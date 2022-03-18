29beb1a8e079d1739eb106571116a5332d9a1b31
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-18 21:55:02 +0800

    Define activity

--- pages/AC_detail/AC_detail.js
+++ pages/AC_detail/AC_detail.js
@@ -1,5 +1,20 @@
 // pages/AC_detail/AC_detail.js
 Page({
+  data:{
+      clubName: null,
+      publisher: null,
+      beginTime: null,
+      endTime: null,
+      endTime: null,
+      activityTitle:null,
+      activityDesc:null,
+      building:null,
+      room:null,
+      estimateNum:0,
+      targetPeople:null,
+      registryNum:0,
+      hot:0,
+  },
 
   go_to_revise1 (){
     wx.navigateTo({
