68d7ecb5526adce9afe8e8173c84ecc0ec511a46
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-18 21:22:21 +0800

    Initial

--- pages/revise1/revise1.js
+++ pages/revise1/revise1.js
@@ -1,71 +1,10 @@
 // pages/revise1/revise1.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-    /**
- * 这是修改板块第一页next>按钮绑定的bindtap事件
- */
   go_to_revise2(){
     wx.navigateTo({
       url:'/pages/revise2/revise2',
