0c41580baa7c0e0e12c7472879f8f6096f9a0b21
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-20 10:23:07 +0800

    改变view activity 逻辑

--- pages/activities/mainPage/mainPage.js
+++ pages/activities/mainPage/mainPage.js
@@ -1,125 +1,123 @@
 //index.js
-//获取应用实例
-const $vm = getApp()
 
-const cache = Object.create(null)
-
-const {decodeHtml,parseNews} = $vm.utils
-
-
+//var util = require('../../../utils/util.js')
+//var app = getApp()
 Page({
-    _isLoading:false,
-    data: {
-        swiperList:[],
-        articles:[],
-        categoryTabs:[],
-        currentTab:0
-    },
-    onShow(){
-        if($vm.globalData.categoryChanged){
-            $vm.utils.getCategorys().then(res => this.setData({
-                categoryTabs:res
-            }))
-            $vm.globalData.categoryChanged = false
-        }
-    },
-    onReady(){
-        this.getNewsList()
-    },
-    onLoad: function() {
+  data: {
+    feed: [],
+    feed_length: 0
+  },
+  //事件处理函数
+  bindItemTap: function() {
+    wx.navigateTo({
+      url: '../answer/answer'
+    })
+  },
+  bindQueTap: function() {
+    wx.navigateTo({
+      url: '../question/question'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.getData();
+  },
+  upper: function () {
+   //现在是发布新活动的按钮。
 
-    },
+   //导向活动发布页1
+   wx.navigateTo({
+    url: '/pages/AC_upload1/AC_upload1',
+  })
 
-    // 页面相关事件处理函数--监听用户下拉动作，下拉刷新
-    onPullDownRefresh(){
-        // 刷新页面，清空当前的缓存，重新获取
-        var chid = this.data.currentTab
-        // 命中缓存
-        if(cache[chid]){
-            cache[chid] = {slides:[],news:[],page:0,time:Date.now()}
-        }
-        this.getNewsList(chid)
-    },
-    // 到达底部，重新加载
-    onReachBottom(){
-        this.getNewsList(this.data.currentTab,1)
-    },
-    // 切换当前选择的分类
-    changeCategory(event){
-        var chid = event.target.dataset.id
-        // 获取ccurrentTab.没有切换分类
-        if(this.data.currentTab === chid){
-            return false
-        }
-        this.setData({ currentTab:chid })
-        this.getNewsList(chid,0)
-    },
-    getNewsList(chid = 0,page = 0){
-        
-        if(!cache[chid]){
-            // 新内容
-            cache[chid] = {slides:[],news:[],page:0,time:Date.now()}
-        }
-        var infos = cache[chid]
-        // 获取下一页数据
-        if(page){
-            // 加载中。无法触发
-            if(this._isLoading){
-                return false;
-            }
-            infos.page += 1
-        }else{
-            // 直接从缓存中取出
-            if(infos.news.length){
-                this.setData({
-                    swiperList:infos.slides,
-                    articles:infos.news
-                })
-                return false
-            }
-        }
+   
+   
+   
+    //原用于刷新
+    // wx.showNavigationBarLoading()
+    // this.refresh();
+    // console.log("upper");
+    // setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
+  },
+  lower: function (e) {
+    wx.showNavigationBarLoading();
+    var that = this;
+    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
+    console.log("lower")
+  },
+  //scroll: function (e) {
+  //  console.log("scroll")
+  //},
 
-        
+  //网络请求数据, 实现首页刷新
+  refresh0: function(){
+    var index_api = '';
+    util.getData(index_api)
+        .then(function(data){
+          //this.setData({
+          //
+          //});
+          console.log(data);
+        });
+  },
 
-        this._isLoading = true
-        
-        $vm.utils.post('v2/news/list.html',{chid:chid,page:infos.page}).then(res => {
-            this._isLoading = false
+  //使用本地 fake 数据实现刷新效果
+  getData: function(){
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+  },
+  refresh: function(){
+    wx.showToast({
+      title: '刷新中',
+      icon: 'loading',
+      duration: 3000
+    });
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+    setTimeout(function(){
+      wx.showToast({
+        title: '刷新成功',
+        icon: 'success',
+        duration: 2000
+      })
+    },3000)
 
-            var {code,newsList,newsBanner} = res
-            if(code === 0){
-                // 新闻管理
-                // style 3下面三张图，1,下面一张大图，2.普通模式，0.无图模式
-                if(newsList && newsList.length){
-                    // infos.news = []
-                    var datas = parseNews(newsList)
-                    infos.news.push(...datas)
-                }
-                // 轮播管理
-                if(newsBanner && newsBanner.length){
-                    // infos.slides = []
-                    var banners = newsBanner.map(news => {
-                        return {
-                            id:news.news_id,
-                            title:news.news_title,
-                            image:news.news_icon.pop(),
-                            style:news.news_style
-                        }
-                    })
-                    infos.slides.push(...banners)
-                }
-                this.setData({
-                    swiperList:infos.slides,
-                    articles:infos.news
-                })
-            }
-            
-        }).catch(err =>console.log(err))
+  },
 
-    },
-    manageTabs(){
-        wx.navigateTo({
-            url:'/pages/news/manage'
-        })
-    }
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function(){
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 4000
+    })
+    var next = util.getNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
+    setTimeout(function(){
+      wx.showToast({
+        title: '加载成功',
+        icon: 'success',
+        duration: 2000
+      })
+    },3000)
+  }
+
+
 })
