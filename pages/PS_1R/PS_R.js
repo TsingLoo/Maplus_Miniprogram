326d41195fa37585be18186f26bbed6efaa0d1ab
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-03-27 10:33:42 +0800

    非法字符检验

--- pages/PS_1R/PS_R.js
+++ pages/PS_1R/PS_R.js
@@ -7,12 +7,46 @@ userPassword:""
 },
 
 inputuserName:function(e){
-this.setData({
-userName:e.detail.value
-})
-console.log("userName: " + this.data.userName)
+  this.setData({
+    userName:e.detail.value
+  })
+  console.log("userName: " + this.data.userName)
+
+  wx.request({
+    url: 'http://localhost:8080/userCheck',
+    method: 'POST',
+    data:
+    {
+      userName:  this.data.userName
+    },
+    success:function(res)
+    {
+      console.log(res.data);
+    }
+  })
+
+
+
 },
   
+CheckForm:function(){undefined
+ 
+
+ if(contain(oContain.value," ^[^!@#$%^&*.()-=+]+$ ")){undefined
+    //非法
+ alert("对不起!\n您输入的是非法字符!请重新输入!");
+  return false;
+}else{undefined
+ //合法
+ alert("恭喜您!\n输入的字符合法!");
+  return true;
+
+}
+
+},
+
+
+
 inputuserNickname:function(e){
 this.setData({
 userNickname:e.detail.value
@@ -38,32 +72,32 @@ icon:'error',
 duration:1000
 }) 
 }
-wx.request({
-url: 'http://localhost:8080/userRegister',
-method: 'post',
-data:{
-userName:this.data.userName, 
-userNickname:this.data.userNickname,
-userPassword:this.data.userPassword
-},
-})
-wx.request({
-url: 'http://localhost:8080/userRegister',
-method: 'get',
-})
-var a = this.data.request;
-if(a == true){
-wx.showToast({
-title: '注册成功！',
-icon:'success',
-duration:1000
-})
-}else if( a == false){
-wx.showToast({
-title: '该用户名已存在！',
-icon:'error',
-duration:1000
-})
-} 
+// wx.request({
+// url: 'http://localhost:8080/userRegister',
+// method: 'post',
+// data:{
+// userName:this.data.userName, 
+// userNickname:this.data.userNickname,
+// userPassword:this.data.userPassword
+// },
+// })
+// wx.request({
+// url: 'http://localhost:8080/userRegister',
+// method: 'get',
+// })
+// var a = this.data.request;
+// if(a == true){
+// wx.showToast({
+// title: '注册成功！',
+// icon:'success',
+// duration:1000
+// })
+// }else if( a == false){
+// wx.showToast({
+// title: '该用户名已存在！',
+// icon:'error',
+// duration:1000
+// })
+// } 
 }
 })
