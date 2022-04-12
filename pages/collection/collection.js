00300bc2cd2796bd00711307bc1628ac72cace6e
xiaonan <xiaonan.pan19@student.xjtlu.edu.cn>
2022-04-12 15:41:45 +0800

    首页图片、活动版、个人面板报名按钮完成
    解决版本问题

--- pages/collection/collection.js
+++ pages/collection/collection.js
@@ -1,37 +1,60 @@
+// pages/collection/collection.js
 var util = require('../../utils/util.js')
 var app = getApp()
 
 Page({
-  data: {
-    activityId:'',
-    articleTitle:'',
-    activityDesc:'',
-    hot:'',
-    },
 
-    get: function () {
-      var that = this;
-      wx.request({
-      url:'//localhost:8080/Staractivity/{username}',
-      method:'GET',
-      success: function(res) {
-      var articleTitle = res.data.titValue;
-      var activityDesc = res.data.activityDescValue;
-      this.data.titValue = articleTitle;
-      this.data.activityDescValue = activityDesc;
-      }
-      })
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    feed: [],
+    feed_length: 0
+
+  },
+
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.refresh();
+  },
+
+
+
+  bindItemTap:function() {
+    wx.navigateTo({
+      url: '/pages/AC_detail/AC_detail',
+    }) 
+  },
+
+   //使用本地 fake 数据实现刷新效果
+  refresh: function(){
+    var feed = util.getCollection();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+  },
+
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function(){
+    var next = util.collectionNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
+  }
+
   
-     wx.request({
-     url: '//localhost:8080/Staractivity/{username}',
-     method:'GET',
-     success: function(res){
-     var hot = res.data.hot;
-     this.data.hot = hot;
-     }
-     })
-  
-    }
 
 
 })
