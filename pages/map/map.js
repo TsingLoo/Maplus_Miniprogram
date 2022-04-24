9131882d20059221642123b1c2c4ba1b033bb906
chuan-wa <1379499077@qq.com>
2022-04-24 11:51:35 +0800

    map实装，marker演示，范围限制为校区，调试库需调整到2.22以上

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,108 +1,120 @@
-// pages/map/map.js
 Page({
-
+  onShareAppMessage() {
+    return {
+      title: 'map',
+      path: 'packageComponent/pages/map/map/map'
+    }
+  },
 
   data: {
-    stv: {
-      offsetX: 0,
-      offsetY: 0,
-      zoom: false, //是否缩放状态
-      distance: 0,  //两指距离
-      scale: 1,  //缩放倍数
-    }
+    
+    theme: 'light',
+    latitude: 31.275,
+    longitude: 120.74,
+    markers: [{
+      id:1,
+      latitude: 31.274659,
+      longitude: 120.738168,
+      width:30,
+      height:30,
+      callout:{
+        content:"list of activities in FB"
+      }
+
+    }],
+    
+    subKey: '4EYBZ-7Y7Y5-EQRIG-QUCXB-GDWCE-3IFDN',
+    enable3d: false,
+    showCompass: false,
+    enableOverlooking: false,
+    enableZoom: true,
+    enableScroll: true,
+    enableRotate: false,
+    drawPolygon: false,
+    enableSatellite: false,
+    enableTraffic: false
+  },
+  toggle3d() {
+    this.setData({
+      enable3d: !this.data.enable3d
+    })
+  },
+  toggleShowCompass() {
+    this.setData({
+      showCompass: !this.data.showCompass
+    })
+  },
+  toggleOverlooking() {
+    this.setData({
+      enableOverlooking: !this.data.enableOverlooking
+    })
+  },
+  toggleZoom() {
+    this.setData({
+      enableZoom: !this.data.enableZoom
+    })
+  },
+  toggleScroll() {
+    this.setData({
+      enableScroll: !this.data.enableScroll
+    })
+  },
+  toggleRotate() {
+    this.setData({
+      enableRotate: !this.data.enableRotate
+    })
+  },
+  togglePolygon() {
+    this.setData({
+      drawPolygon: !this.data.drawPolygon
+    })
+  },
+  toggleSatellite() {
+    this.setData({
+      enableSatellite: !this.data.enableSatellite
+    })
+  },
+  toggleTraffic() {
+    this.setData({
+      enableTraffic: !this.data.enableTraffic
+    })
   },
 
+    
 
-  //事件处理函数
-  touchstartCallback: function(e) {
-
-    wx.getLocation({
-      success: function(res){
-        console.log(res.latitude);
-        console.log(res.longitude);
-      }
+  onLoad:function() {
+    this.mapCtx=wx.createMapContext('map')
+    this.mapCtx.setBoundary({
+      southwest:{
+        longitude:120.736773,
+        latitude:31.267817,
+      },
+      northeast:{
+        longitude:120.743739,
+        latitude:31.277354,
+      },
+      complete: function(e){},
+    },
+    
+    )
+    this.mapCtx.addGroundOverlay({
+      id:0,
+      src:"/icon/activity.png",
+      bounds:{
+        southwest:{
+          longitude:113,
+          latitude:23,
+        },
+        northeast:{
+          longitude:113.002,
+          latitude:23.002,
+        }
+      },
+      complete: function(e){},
     })
 
-    //触摸开始
-    console.log('touchstartCallback');
-    console.log(e);
 
+    
 
-    if (e.touches.length === 1) {
-      let {clientX, clientY} = e.touches[0];
-      this.startX = clientX;
-      this.startY = clientY;
-      this.touchStartEvent = e.touches;
-    } else {
-      let xMove = e.touches[1].clientX - e.touches[0].clientX;
-      let yMove = e.touches[1].clientY - e.touches[0].clientY;
-      let distance = Math.sqrt(xMove * xMove + yMove * yMove);
-      this.setData({
-        'stv.distance': distance,
-        'stv.zoom': true, //缩放状态
-      })
-    }
-
-  },
-  touchmoveCallback: function(e) {
-    // //触摸移动中
-    // console.log('touchmoveCallback');
-    // console.log(e);
-    var windowHeight = wx.getSystemInfoSync().windowHeight;
-    var windowWidth = wx.getSystemInfoSync().windowWidth;
-
- 
-    if (e.touches.length === 1) {
-      //单指移动
-      if (this.data.stv.zoom) {
-        //缩放状态，不处理单指
-        return ;
-      }
-      
-
-
-      let {clientX, clientY} = e.touches[0];
-      let offsetX = clientX - this.startX;
-      let offsetY = clientY- this.startY;
-      this.startX = clientX;
-      this.startY = clientY;
-      let {stv} = this.data;
-      stv.offsetX += offsetX;
-      stv.offsetY += offsetY;
-      stv.offsetLeftX = -stv.offsetX;
-      stv.offsetLeftY = -stv.offsetLeftY;
-      this.setData({
-        stv: stv
-      });
-
-    } else {
-      //双指缩放
-      let xMove = e.touches[1].clientX - e.touches[0].clientX;
-      let yMove = e.touches[1].clientY - e.touches[0].clientY;
-      let distance = Math.sqrt(xMove * xMove + yMove * yMove);
-
-      let distanceDiff = distance - this.data.stv.distance;
-      let newScale = this.data.stv.scale + 0.005 * distanceDiff;
-
-      this.setData({
-        'stv.distance': distance,
-        'stv.scale': newScale,
-      })
-    }
-
-  },
-  touchendCallback: function(e) {
-    //触摸结束
-    console.log('touchendCallback');
-    console.log(e);
-
-    if (e.touches.length === 0) {
-      this.setData({
-        'stv.zoom': false, //重置缩放状态
-      })
-    }
-  },
-  onLoad: function () {
-    console.log('onLoad');
   }
 })
