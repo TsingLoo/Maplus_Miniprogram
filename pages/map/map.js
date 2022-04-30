740ee5fe8f8e3a470f8857229035525ccd467f1c
chuan-wa <1379499077@qq.com>
2022-04-30 11:54:52 +0800

    地图显示无缝

--- pages/map/map.js
+++ pages/map/map.js
@@ -5,12 +5,69 @@ Page({
       path: 'packageComponent/pages/map/map/map'
     }
   },
+  getScale:function(){
+    this.mapCtx.getScale({
+      success:function(res){
+      console.log(res.scale)
+      }
+    })
+  },
+
+  onLoad:function() {
+    this.mapCtx=wx.createMapContext('map')
+    
+    this.mapCtx.setBoundary({
+      southwest:{
+        longitude:120.734742,
+        latitude:31.268347,
+      },
+      northeast:{
+        longitude:120.746592,
+        latitude:31.279598,
+      },
+      
+      complete: function(e){},
+    },
+    
+    )
+    this.mapCtx.addGroundOverlay({
+      id:0,
+      src:"/icon/map.jpg",
+      bounds:{
+        southwest:{
+          longitude:120.734742,
+          latitude:31.268347,
+        },
+        northeast:{
+          longitude:120.746592,
+          latitude:31.279598,
+        }
+      },
+      complete: function(e){},
+    }) 
+
+    this.mapCtx.moveToLocation({
+      latitude: 31.274659,
+      longitude: 120.738168,
+      complete: function(e){},
+
+    })
+    
+
+    
+},
 
   data: {
     
     theme: 'light',
-    latitude: 31.275,
-    longitude: 120.74,
+    latitude: 31.274659,
+    longitude: 120.738168,
+    minScale:16.135946271878986,
+    maxScale:18,
+    showLocation:false,
+
+    scale:17,
+    
     markers: [{
       id:1,
       latitude: 31.274659,
@@ -21,11 +78,22 @@ Page({
         content:"list of activities in FB"
       }
 
+    },{
+      id:2,
+      latitude:31.268947,
+      longitude:120.738650,
+      width:30,
+      height:30,
+      callout:{
+        content:"test marker"
+      }
+
     }],
     
     subKey: '4EYBZ-7Y7Y5-EQRIG-QUCXB-GDWCE-3IFDN',
     enable3d: false,
     showCompass: false,
+    
     enableOverlooking: false,
     enableZoom: true,
     enableScroll: true,
@@ -81,39 +149,7 @@ Page({
     })
   },
 
-    
 
-  onLoad:function() {
-    this.mapCtx=wx.createMapContext('map')
-    this.mapCtx.setBoundary({
-      southwest:{
-        longitude:120.736773,
-        latitude:31.267817,
-      },
-      northeast:{
-        longitude:120.743739,
-        latitude:31.277354,
-      },
-      complete: function(e){},
-    },
-    
-    )
-    this.mapCtx.addGroundOverlay({
-      id:0,
-      src:"/icon/map.jpg",
-      bounds:{
-        southwest:{
-          longitude:120.736773,
-          latitude:31.267817,
-        },
-        northeast:{
-          longitude:120.743739,
-          latitude:31.277354,
-        }
-      },
-      complete: function(e){},
-    }) 
-    
 
-  }
+  
 })
